e74ec558561329910675bd44e6895fae60bbc0f9
jcampelo <jcampelofisica@gmail.com>
2026-07-17 14:45:44 -0300

    Atualizado main_file.py com alteração anterior e adição de valor médio ao gráfico.

--- main_file.py
+++ main_file.py
@@ -11,13 +11,27 @@ from datetime import date
 df = pd.read_csv('PC_Gamer.csv', sep=';')
 
 
+###############
+#### LINKS ####
+###############
+
+cpu_link = 'https://www.amazon.com.br/Processador-AMD-Ryzen-5600GT-Threads/dp/B0CQ4DTJYX/ref=asc_df_B0CQ4DTJYX?mcid=ee61770518f834679b2c75152745b69b&tag=googleshopp00-20&linkCode=df0&hvadid=709884378235&hvpos=&hvnetw=g&hvrand=14881669649454203673&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9101448&hvtargid=pla-2280624564623&psc=1&language=pt_BR&gad_source=1'
+motherboard_link = 'https://www.terabyteshop.com.br/produto/18646/placa-mae-msi-b550m-a-pro-chipset-b550-amd-am4-matx-ddr4?gad_source=1&gad_campaignid=16136003025&gclid=CjwKCAjw-dfOBhAjEiwAq0RwIz8B9H8t5nWpALxwlzeNSRzDe_0N0SyEK5sfVcw5-6LGCgzhKTTASBoCiYIQAvD_BwE'
+ssd_link = 'https://www.kabum.com.br/produto/536953/ssd-sandisk-sn350-1tb-m-2-2280-pcie-nvme-leitura-2400mb-s-e-gravacao-1850mb-s-verde-wds100t2g0c'
+ram_link = 'https://pt.aliexpress.com/item/1005012323512000.html?pdp_ext_f=%7B"sku_id"%3A"12000058084377368"%7D&sourceType=1&spm=a2g0o.wish-manage-home.0.0&gatewayAdapt=glo2bra'
+gabinete_link = 'https://www.kabum.com.br/produto/887046/gabinete-gaming-c3tech-aquarius-com-vidro-temperado-sem-fonte-mt-g860bk-'
+mouse_link = 'https://pt.aliexpress.com/item/1005012371400759.html?spm=a2g0o.productlist.main.23.459c25f81bcfCL&algo_pvid=fe26c5bb-67a9-42a2-8835-77951b1c0678&algo_exp_id=fe26c5bb-67a9-42a2-8835-77951b1c0678-20&pdp_ext_f=%7B"order"%3A"23"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21336.34%21147.99%21%21%21407.73%21179.40%21%402101f70717832764055654429e72c1%2112000058191763437%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895&curPageLogUid=Lq0Biyq3hzZX&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005012371400759%7C_p_origin_prod%3A'
+headset_link = 'https://pt.aliexpress.com/item/1005009495386384.html?aff_fcid=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&tt=CPS_NORMAL&aff_fsk=_c3gITj9D&aff_platform=shareComponent-detail&sk=_c3gITj9D&aff_trace_key=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&terminal_id=d874b61df0ea490bb40119dc36685fe8&afSmartRedirect=y'
+soundbar_link = 'https://pt.aliexpress.com/item/1005006436035998.html?spm=a2g0o.productlist.main.2.26c7y0DSy0DSur&algo_pvid=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a&algo_exp_id=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a-1&pdp_ext_f=%7B"order"%3A"4969"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21229.12%21100.55%21%21%21287.08%21125.98%21%402103212b17798072772552193e62f7%2112000037173402947%21sea%21BR%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Aeaf596e%3Bm03_new_user%3A-29895%3BpisId%3A5000000207258923&curPageLogUid=jr5V9vRJV1Bl&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006436035998%7C_p_origin_prod%3A'
+
+
 
 #####################
 #### PROCESSADOR ####
 #####################
 
 driver = webdriver.Chrome()
-driver.get('https://www.amazon.com.br/Processador-AMD-Ryzen-5600GT-Threads/dp/B0CQ4DTJYX/ref=asc_df_B0CQ4DTJYX?mcid=ee61770518f834679b2c75152745b69b&tag=googleshopp00-20&linkCode=df0&hvadid=709884378235&hvpos=&hvnetw=g&hvrand=14881669649454203673&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9101448&hvtargid=pla-2280624564623&psc=1&language=pt_BR&gad_source=1')
+driver.get(cpu_link)
 
 driver.implicitly_wait(10)
 
@@ -63,7 +77,7 @@ print("Preço CPU adicionado!")
 #### MOTHERBOARD ####
 #####################
 
-driver.get('https://www.terabyteshop.com.br/produto/18646/placa-mae-msi-b550m-a-pro-chipset-b550-amd-am4-matx-ddr4?gad_source=1&gad_campaignid=16136003025&gclid=CjwKCAjw-dfOBhAjEiwAq0RwIz8B9H8t5nWpALxwlzeNSRzDe_0N0SyEK5sfVcw5-6LGCgzhKTTASBoCiYIQAvD_BwE')
+driver.get(motherboard_link)
 driver.implicitly_wait(10)
 
 mboard_ptbr = driver.find_element(By.ID, 'valParc').get_property('innerText')
@@ -91,14 +105,14 @@ print("Preço Placa-mãe adicionado!")
 #### SSD ####
 #############
 
-driver.get('https://pt.aliexpress.com/item/1005006783107881.html?sourceType=620&channel=coin&improveDiscount=Y&BuyNow=true&utm=iskandarsouzo_sophie&aff_fcid=776b93166200483383f7ee2b8c7a626b-1783267233933-06465-_c3nNHgTr&tt=API&aff_fsk=_c3nNHgTr&afSmartRedirect=y&dp=gx-br-aliexpress-aliexpress-cos&aff_fcid=158f311f0235481b9d6724a876e86c26-1783274078397-07732-_c3IGhl7b&tt=CPS_NORMAL&aff_fsk=_c3IGhl7b&aff_platform=portals-tool&sk=_c3IGhl7b&aff_trace_key=158f311f0235481b9d6724a876e86c26-1783274078397-07732-_c3IGhl7b&terminal_id=e7a44edf1bc34d368a13eb65be5c194e&afSmartRedirect=y')
+driver.get(ssd_link)
 driver.implicitly_wait(10)
 
-button = driver.find_element(By.XPATH, "//div[@title='1TB']")
+button = driver.find_element(By.ID, "adopt-accept-all-button")
 button.click()
 
-ssd_price_ptbr = driver.find_element(By.XPATH, "//div[@class='price-default--currentWrap--A_MNgCG']").get_property('innerText')
-ssd_price = ssd_price_ptbr.split('$')[1].replace(',', '.')
+ssd_price_ptbr = driver.find_element(By.XPATH, "(//div[@class='w-full']/span/b)").get_property('innerText')
+ssd_price = ssd_price_ptbr.split('\xa0')[1].replace('.', '').replace(',', '.')
 ssd_price = float(ssd_price)
 print(f"Preço SSD: {ssd_price}")
 
@@ -122,7 +136,7 @@ print("Preço SSD adicionado!")
 #### RAM ####
 #############
 
-driver.get('https://pt.aliexpress.com/item/1005012323512000.html?pdp_ext_f=%7B"sku_id"%3A"12000058084377368"%7D&sourceType=1&spm=a2g0o.wish-manage-home.0.0&gatewayAdapt=glo2bra')
+driver.get(ram_link)
 driver.implicitly_wait(10)
 
 ram_price_ptbr = driver.find_element(By.XPATH, "//div[@class='price-default--currentWrap--A_MNgCG']").get_property('innerText')
@@ -150,11 +164,11 @@ print("Preço RAM adicionado!")
 #### GABINETE ####
 ##################
 
-driver.get('https://www.kabum.com.br/produto/887046/gabinete-gaming-c3tech-aquarius-com-vidro-temperado-sem-fonte-mt-g860bk-')
+driver.get(gabinete_link)
 driver.implicitly_wait(10)
 
-button = driver.find_element(By.ID, "adopt-accept-all-button")
-button.click()
+#button = driver.find_element(By.ID, "adopt-accept-all-button")
+#button.click()
 
 gabinete_ptbr = driver.find_element(By.XPATH, "(//div[@class='w-full']/span/b)").get_property('innerText')
 gabinete_price = gabinete_ptbr.split('\xa0')[1].replace(',', '.')
@@ -181,7 +195,7 @@ print("Preço Gabinete adicionado!")
 #### MOUSE ####
 ###############
 
-driver.get('https://pt.aliexpress.com/item/1005012371400759.html?spm=a2g0o.productlist.main.23.459c25f81bcfCL&algo_pvid=fe26c5bb-67a9-42a2-8835-77951b1c0678&algo_exp_id=fe26c5bb-67a9-42a2-8835-77951b1c0678-20&pdp_ext_f=%7B"order"%3A"23"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21336.34%21147.99%21%21%21407.73%21179.40%21%402101f70717832764055654429e72c1%2112000058191763437%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895&curPageLogUid=Lq0Biyq3hzZX&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005012371400759%7C_p_origin_prod%3A')
+driver.get(mouse_link)
 driver.implicitly_wait(10)
 
 
@@ -210,7 +224,7 @@ print("Preço Mouse adicionado!")
 #### HEADSET ####
 #################
 
-driver.get('https://pt.aliexpress.com/item/1005009495386384.html?aff_fcid=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&tt=CPS_NORMAL&aff_fsk=_c3gITj9D&aff_platform=shareComponent-detail&sk=_c3gITj9D&aff_trace_key=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&terminal_id=d874b61df0ea490bb40119dc36685fe8&afSmartRedirect=y')
+driver.get(headset_link)
 driver.implicitly_wait(10)
 
 button = driver.find_element(By.XPATH, "//div[@data-sku-col='14-193']")
@@ -242,7 +256,7 @@ print("Preço Headset adicionado!")
 #### SOUNDBAR ####
 ##################
 
-driver.get('https://pt.aliexpress.com/item/1005006436035998.html?spm=a2g0o.productlist.main.2.26c7y0DSy0DSur&algo_pvid=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a&algo_exp_id=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a-1&pdp_ext_f=%7B"order"%3A"4969"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21229.12%21100.55%21%21%21287.08%21125.98%21%402103212b17798072772552193e62f7%2112000037173402947%21sea%21BR%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Aeaf596e%3Bm03_new_user%3A-29895%3BpisId%3A5000000207258923&curPageLogUid=jr5V9vRJV1Bl&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006436035998%7C_p_origin_prod%3A')
+driver.get(soundbar_link)
 driver.implicitly_wait(10)
 
 soundbar_ptbr = driver.find_element(By.XPATH, "(//div[@class='price-default--currentWrap--A_MNgCG'])").get_property('innerText')
@@ -314,17 +328,32 @@ cores = ['black', 'blue', 'red', 'green', 'magenta', 'orange', 'darkviolet', 'fi
 for idx, (item, df_item) in enumerate(df_itens.items()):
     if idx < len(axes):
         cor = cores[idx % len(cores)]
-
+        
+        # Calcular o valor médio do preço para este item
+        valor_medio = df_item['Preco'].mean()
+        
+        # Plotar os dados
         axes[idx].plot(df_item['Data'], df_item['Preco'], 
                        marker='o', 
                        linewidth=2, 
                        markersize=6,
-                       color=cor)
+                       color=cor,
+                       label=f'{item}')
+        
+        # Adicionar a reta do valor médio (tracejada, preta)
+        axes[idx].axhline(y=valor_medio, 
+                          color='gray', 
+                          linestyle='--', 
+                          linewidth=2,
+                          alpha=0.7,
+                          label=f'Média: R$ {valor_medio:.2f}')
+        
         axes[idx].set_title(f'{item}', fontsize=12, fontweight='bold')
         axes[idx].set_xlabel('Data', fontsize=10)
         axes[idx].set_ylabel('Preço (R$)', fontsize=10)
         axes[idx].grid(True, alpha=0.3)
         axes[idx].tick_params(axis='x', rotation=30)
+        axes[idx].legend(loc='best', fontsize=8)
 
 # Remover subplots vazios (se houver menos de 8 itens)
 for idx in range(len(df_itens), len(axes)):
@@ -332,6 +361,7 @@ for idx in range(len(df_itens), len(axes)):
 
 plt.tight_layout()
 
+# Salvar a figura
 plt.savefig('plots/pcgamer_prices.png', dpi=300)
 
 plt.show()
