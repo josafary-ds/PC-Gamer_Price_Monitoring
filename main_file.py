d4ed0ccfc6bad566dcb0e0627270e630b0bdb635
jcampelo <jcampelofisica@gmail.com>
2026-08-16 14:43:55 -0300

    Substituido todos os dados de RAM por Keyboard.

--- main_file.py
+++ main_file.py
@@ -18,11 +18,11 @@ df = pd.read_csv('PC_Gamer.csv', sep=';')
 cpu_link = 'https://www.amazon.com.br/gp/aw/d/B0CQ4DTJYX?openid.assoc_handle=bramazon&openid.claimed_id=https%3A%2F%2Fwww.amazon.com.br%2Fap%2Fid%2Famzn1.account.AFSLCZAT6KPJPJL2MNOJJLJ4UUPA&openid.identity=https%3A%2F%2Fwww.amazon.com.br%2Fap%2Fid%2Famzn1.account.AFSLCZAT6KPJPJL2MNOJJLJ4UUPA&openid.mode=id_res&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.op_endpoint=https%3A%2F%2Fwww.amazon.com.br%2Fap%2Fsignin&openid.response_nonce=2026-08-01T19%3A18%3A36Z-7015999307553914865&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2Fgp%2Faw%2Fd%2FB0CQ4DTJYX&openid.signed=assoc_handle%2Cclaimed_id%2Cidentity%2Cmode%2Cns%2Cop_endpoint%2Cresponse_nonce%2Creturn_to%2Cns.pape%2Cpape.auth_policies%2Cpape.auth_time%2Csigned&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.auth_policies=http%3A%2F%2Fschemas.openid.net%2Fpape%2Fpolicies%2F2007%2F06%2Fnone&openid.pape.auth_time=2026-08-01T19%3A18%3A36Z&openid.sig=vZYwcxsW9yDyHL7oq%2FVX8u6giWcqbwWnMK7BYFzHluM%3D&serial='
 motherboard_link = 'https://www.terabyteshop.com.br/produto/18646/placa-mae-msi-b550m-a-pro-chipset-b550-amd-am4-matx-ddr4?gad_source=1&gad_campaignid=16136003025&gclid=CjwKCAjw-dfOBhAjEiwAq0RwIz8B9H8t5nWpALxwlzeNSRzDe_0N0SyEK5sfVcw5-6LGCgzhKTTASBoCiYIQAvD_BwE'
 ssd_link = 'https://www.kabum.com.br/produto/626785/ssd-pcie-husky-thunderboost-512gb-m-2-nvme-leitura-2200mb-s-gravacao-1600mb-s-preto-hssd002512'
-ram_link = 'https://pt.aliexpress.com/item/1005012323512000.html?pdp_ext_f=%7B"sku_id"%3A"12000058084377368"%7D&sourceType=1&spm=a2g0o.wish-manage-home.0.0&gatewayAdapt=glo2bra'
 gabinete_link = 'https://www.kabum.com.br/produto/887046/gabinete-gaming-c3tech-aquarius-com-vidro-temperado-sem-fonte-mt-g860bk-'
 mouse_link = 'https://pt.aliexpress.com/item/1005012371400759.html?spm=a2g0o.productlist.main.23.459c25f81bcfCL&algo_pvid=fe26c5bb-67a9-42a2-8835-77951b1c0678&algo_exp_id=fe26c5bb-67a9-42a2-8835-77951b1c0678-20&pdp_ext_f=%7B"order"%3A"23"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21336.34%21147.99%21%21%21407.73%21179.40%21%402101f70717832764055654429e72c1%2112000058191763437%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895&curPageLogUid=Lq0Biyq3hzZX&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005012371400759%7C_p_origin_prod%3A'
 headset_link = 'https://pt.aliexpress.com/item/1005009495386384.html?aff_fcid=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&tt=CPS_NORMAL&aff_fsk=_c3gITj9D&aff_platform=shareComponent-detail&sk=_c3gITj9D&aff_trace_key=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&terminal_id=d874b61df0ea490bb40119dc36685fe8&afSmartRedirect=y'
 soundbar_link = 'https://pt.aliexpress.com/item/1005006436035998.html?spm=a2g0o.productlist.main.2.26c7y0DSy0DSur&algo_pvid=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a&algo_exp_id=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a-1&pdp_ext_f=%7B"order"%3A"4969"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21229.12%21100.55%21%21%21287.08%21125.98%21%402103212b17798072772552193e62f7%2112000037173402947%21sea%21BR%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Aeaf596e%3Bm03_new_user%3A-29895%3BpisId%3A5000000207258923&curPageLogUid=jr5V9vRJV1Bl&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006436035998%7C_p_origin_prod%3A'
+keyboard_link = 'https://pt.aliexpress.com/item/1005006982966199.html?spm=a2g0o.productlist.main.5.4fd0cWjAcWjAVG&algo_pvid=dd974723-a0f0-40aa-bca7-09a727a07434&algo_exp_id=dd974723-a0f0-40aa-bca7-09a727a07434-4&pdp_ext_f=%7B"order"%3A"1436"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21369.65%21174.62%21%21%21443.60%21209.56%21%402101e5ab17868916822482882e0eee%2112000038938334333%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895%3BpisId%3A5000000214354137&curPageLogUid=9VrS02B33GmJ&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006982966199%7C_p_origin_prod%3A'
 
 
 
@@ -129,34 +129,6 @@ new_data = pd.DataFrame([{
 
 df = pd.concat([df, new_data], ignore_index=True)
 print("Preço SSD adicionado!")
-
-
-
-#############
-#### RAM ####
-#############
-
-driver.get(ram_link)
-driver.implicitly_wait(10)
-
-ram_price_ptbr = driver.find_element(By.XPATH, "//div[@class='price-default--currentWrap--A_MNgCG']").get_property('innerText')
-ram_price = ram_price_ptbr.split('$')[1].replace(',', '.')
-ram_price = float(ram_price)
-print(f"Preço RAM: {ram_price}")
-
-item = 'RAM'
-descricao = '1x JUHOR DDR4 8GB 3200MHz CL16'
-data = date.today()
-
-new_data = pd.DataFrame([{
-    'Item': item,
-    'Descricao': descricao,
-    'Preco': ram_price,
-    'Data': data
-}])
-
-df = pd.concat([df, new_data], ignore_index=True)
-print("Preço RAM adicionado!")
 
 
 
@@ -277,6 +249,38 @@ new_data = pd.DataFrame([{
 
 df = pd.concat([df, new_data], ignore_index=True)
 print("Preço Soundbar adicionado!")
+
+
+#################
+#### TECLADO ####
+#################
+
+driver.get(keyboard_link)
+driver.implicitly_wait(10)
+
+button_close = driver.find_element(By.XPATH, "//div[@class='baxia-dialog-close']")
+button_close.click()
+button_option = driver.find_element(By.XPATH, "//div[@class='sku-item--showHot--hRxq3Vm sku-item--image--jMUnnGA']")
+button_option.click()
+
+keyboard_ptbr = driver.find_element(By.XPATH, "//div[@class='price-default--currentWrap--A_MNgCG']").get_property('innerText')
+keyboard_price = keyboard_ptbr.split('$')[1].replace(',', '.')
+keyboard_price = float(keyboard_price)
+print(f"Preço Teclado: {keyboard_price}")
+
+item = 'Teclado'
+descricao = 'M96 teclado bluetooth'
+data = date.today()
+
+new_data = pd.DataFrame([{
+    'Item': item,
+    'Descricao': descricao,
+    'Preco': keyboard_price,
+    'Data': data
+}])
+
+df = pd.concat([df, new_data], ignore_index=True)
+print("Preço Teclado adicionado!")
 
 
 
