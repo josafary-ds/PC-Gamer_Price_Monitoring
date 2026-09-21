b40b187a669ebfde584da618e4c224e4f436f207
jcampelo <jcampelofisica@gmail.com>
2026-09-21 00:42:34 -0300

    Alterado link do teclado e corrigido xpath da cpu.

--- main_file.py
+++ main_file.py
@@ -23,7 +23,7 @@ mouse_link = 'https://pt.aliexpress.com/item/1005012371400759.html?spm=a2g0o.pro
 headset_link = 'https://pt.aliexpress.com/item/1005009495386384.html?aff_fcid=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&tt=CPS_NORMAL&aff_fsk=_c3gITj9D&aff_platform=shareComponent-detail&sk=_c3gITj9D&aff_trace_key=24cb7ac286da4a1dac72bd6b45a01629-1779218687223-01059-_c3gITj9D&terminal_id=d874b61df0ea490bb40119dc36685fe8&afSmartRedirect=y'
 soundbar_link = 'https://pt.aliexpress.com/item/1005006436035998.html?spm=a2g0o.productlist.main.2.26c7y0DSy0DSur&algo_pvid=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a&algo_exp_id=a6a82e3c-ebe8-41ee-9da0-1fe9c6c92b4a-1&pdp_ext_f=%7B"order"%3A"4969"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21229.12%21100.55%21%21%21287.08%21125.98%21%402103212b17798072772552193e62f7%2112000037173402947%21sea%21BR%210%21ABX%211%210%21n_tag%3A-29910%3Bd%3Aeaf596e%3Bm03_new_user%3A-29895%3BpisId%3A5000000207258923&curPageLogUid=jr5V9vRJV1Bl&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006436035998%7C_p_origin_prod%3A'
 soundbar_red_link = 'https://pt.aliexpress.com/item/1005004684998034.html?spm=a2g0o.productlist.main.1.2c861240DubsIa&algo_pvid=f81d2e67-40df-4e0d-9058-324f7c10291f&algo_exp_id=f81d2e67-40df-4e0d-9058-324f7c10291f-0&pdp_ext_f=%7B"order"%3A"58"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21380.93%21159.99%21%21%2168.05%2128.58%21%402101e7a317874373845623899e0f2e%2112000030094931478%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895&curPageLogUid=08KMvNGPMgAU&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005004684998034%7C_p_origin_prod%3A'
-keyboard_link = 'https://pt.aliexpress.com/item/1005006982966199.html?spm=a2g0o.productlist.main.5.4fd0cWjAcWjAVG&algo_pvid=dd974723-a0f0-40aa-bca7-09a727a07434&algo_exp_id=dd974723-a0f0-40aa-bca7-09a727a07434-4&pdp_ext_f=%7B"order"%3A"1436"%2C"spu_best_type"%3A"price"%2C"eval"%3A"1"%2C"fromPage"%3A"search"%7D&pdp_npi=6%40dis%21BRL%21369.65%21174.62%21%21%21443.60%21209.56%21%402101e5ab17868916822482882e0eee%2112000038938334333%21sea%21BR%217714416806%21X%211%210%21n_tag%3A-29911%3Bd%3A137eff22%3Bm03_new_user%3A-29895%3BpisId%3A5000000214354137&curPageLogUid=9VrS02B33GmJ&utparam-url=scene%3Asearch%7Cquery_from%3A%7Cx_object_id%3A1005006982966199%7C_p_origin_prod%3A'
+keyboard_link = 'https://www.amazon.com.br/Teclado-Mecânico-Redragon-Kumara-Switch/dp/B0C1QFVYQJ/ref=asc_df_B0C1QFVYQJ?mcid=72fcea083724318fb0a2122c67ecec3d&tag=googleshopp00-20&linkCode=df0&hvadid=709884378379&hvpos=&hvnetw=g&hvrand=9615461458994901117&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001621&hvtargid=pla-2243950074900&psc=1&hvocijid=9615461458994901117-B0C1QFVYQJ-&hvexpln=0&language=pt_BR'
 
 
 
@@ -51,9 +51,9 @@ except NoSuchElementException:
     # Se não encontrou, apenas continua
     print("Botão NÃO encontrado. Continuando execução...")
 
-best_offer = driver.find_element(By.ID, 'best-offer-string-cc').get_property('innerHTML')
+best_offer = driver.find_element(By.XPATH, "//div[@class='a-section apex-core-price-identifier']/span[@class='a-price aok-align-center apex-pricetopay-value']").get_property('innerText')
 
-price_cpu_ptbr = best_offer.split(' ')[1].split(';')[1]
+price_cpu_ptbr = best_offer.split('\n')[0].split('R$')[1]
 cpu_price = price_cpu_ptbr.replace('.', '').replace(',', '.')
 cpu_price = float(cpu_price)
 print(f"Preço CPU: {cpu_price}")
@@ -140,29 +140,13 @@ print("Preço Gabinete adicionado!")
 driver.get(keyboard_link)
 driver.implicitly_wait(10)
 
-try:
-    # Tenta encontrar o elemento com o XPath específico
-    button_close = driver.find_element(By.XPATH, "//div[@class='baxia-dialog-close']")
-    
-    # Se encontrou, clica
-    button_close.click()
-    print("Botão encontrado e clicado com sucesso!")
-    
-except NoSuchElementException:
-    # Se não encontrou, apenas continua
-    print("Botão NÃO encontrado. Continuando execução...")
-
-
-button_option = driver.find_element(By.XPATH, "//div[@class='sku-item--showHot--hRxq3Vm sku-item--image--jMUnnGA']")
-button_option.click()
-
-keyboard_ptbr = driver.find_element(By.XPATH, "//div[@class='price-default--currentWrap--A_MNgCG']").get_property('innerText')
-keyboard_price = keyboard_ptbr.split('$')[1].replace(',', '.')
+keyboard_ptbr = driver.find_element(By.XPATH, "//span[@class='best-offer-name a-text-bold']").get_property('innerText')
+keyboard_price = keyboard_ptbr.split('\xa0')[1].split(' ')[0].replace(',', '.')
 keyboard_price = float(keyboard_price)
 print(f"Preço Teclado: {keyboard_price}")
 
 item = 'Teclado'
-descricao = 'M96 teclado bluetooth'
+descricao = 'Redragon Kumara Wireless'
 data = date.today()
 
 new_data = pd.DataFrame([{
